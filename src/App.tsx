ddc4f6753fd62ca7aeff7dfbc24ea449925382a6
Eagala Zeenath <eagalazeenath.com>
2025-11-13 17:56:09 +0530

    Seven commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './component/Header'
 import Footer from './component/Footer'
-import Para from './component/para'
+import Para from './component/Smallpara'
+import Para2 from './component/Paragraph'
 import './App.css'
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
     <>
       <Header />
       <div className="App">
-        <h1>Welcome to Gupio Company</h1>
+        <h1>Welcome Gupio</h1>
         <Para/>
+        <Para2/>
       </div>
       <Footer />
     </>
---
+++ src/component/Paragraph.tsx
@@ -0,0 +1,4 @@
+import "../App.css"
+export default function para() {
+  return <p className="para">There are difference types of parking service.</p>
+}
